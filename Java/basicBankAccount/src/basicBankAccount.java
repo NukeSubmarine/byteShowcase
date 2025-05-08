78ac2de99261442e2ea4fde1aee5634b0def75ec
FCX <fcx@nukesubmarine.com>
2025-05-08 14:56:08 +0100

    Adding TO DOs

--- Java/basicBankAccount/src/basicBankAccount.java
+++ Java/basicBankAccount/src/basicBankAccount.java
@@ -1,6 +1,13 @@
 public class basicBankAccount {
     public static void main(String[] args) {
 
+        //To know and import scanner class
+
+        //Print out user messages
+
+        //To get from scanner input from the terminal
+
+        //Print out the final message - created account
 
     }
 }
