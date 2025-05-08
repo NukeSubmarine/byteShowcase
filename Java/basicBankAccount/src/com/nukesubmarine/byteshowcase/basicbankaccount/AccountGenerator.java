5b76fc1b2fa8e99b8fc0e5c357cc12da620cb378
FCX <fcx@nukesubmnarine.com>
2025-05-08 18:03:51 +0100

    Adding the interface and updtes

--- Java/basicBankAccount/src/com/nukesubmarine/byteshowcase/basicbankaccount/AccountGenerator.java
+++ Java/basicBankAccount/src/com/nukesubmarine/byteshowcase/basicbankaccount/AccountGenerator.java
@@ -1,4 +1,11 @@
+///  ** Interface ** which defines the contract (no implementation).
+/// ** this is the contract for making Accounts.
+
 package com.nukesubmarine.byteshowcase.basicbankaccount;//Interface com.nukesubmarine.byteshowcase.basicbankaccount.AccountGenerator
 
-public class AccountGenerator {
+import java.util.Scanner;
+import com.nukesubmarine.byteshowcase.basicbankaccount.Account;
+
+public interface AccountGenerator {
+        Account generateAccount(Scanner scanner);
 }
